6eec2795cec91301f9cb1b42d281b62f3bdcf350
KishoreRavi-7280 <kishoresde006@gmail.com>
2025-08-14 10:51:55 +0530

    update the website

--- js/car.js
+++ js/car.js
@@ -1,35 +1,21 @@
-// Toggle menu button
-function toggleMenu() {
+//toggle Menu
+function toggleMenu(){
     const menu = document.querySelector('.menu');
     const nav = document.querySelector('.nav');
-    menu.classList.toggle('active');
-    nav.classList.toggle('active');
+    menu.classList.toggle('active')
+    nav.classList.toggle('active')
 }
 
-// Change the background video when clicking on the gallery
-function changeVideo(names) {
+function changeVideo(name){
     const bgVideoList = document.querySelectorAll('.bg-video');
-       const trailers = document.querySelectorAll('.trailer');
+    const trailers = document.querySelectorAll('.trailer');
     const models = document.querySelectorAll('.model');
 
     bgVideoList.forEach(video => {
-        video.classList.remove('active');
-        if (video.classList.contains(names)) {
-            video.classList.add('active');
+        video.classList.remove('.active');
+        if(video.classList.contains(name)){
+            video.classList.add('active')
         }
     });
 
-    models.forEach(model => {
-        model.classList.remove('active');
-        if (model.classList.contains(names)) {
-            model.classList.add('active');
-        }
-    });
-
-    trailers.forEach(trailer => {
-        trailer.classList.remove('active');
-        if (trailer.classList.contains(names)) {
-            trailer.classList.add('active');
-        }
-    });
 }
