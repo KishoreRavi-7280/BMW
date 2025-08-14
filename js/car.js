cd187af2684db2262e572dceb43f0da9af4ac7aa
KishoreRavi-7280 <kishoresde006@gmail.com>
2025-08-14 10:52:57 +0530

    update

--- js/car.js
+++ js/car.js
@@ -19,3 +19,25 @@ function changeVideo(name){
     });
 
 }
+
+
+//toggle Menu
+// function toggleMenu(){
+//     const menu = document.querySelector('.menu');
+//     const nav = document.querySelector('.nav');
+//     menu.classList.toggle('active')
+//     nav.classList.toggle('active')
+// }
+
+// function changeVideo(name){
+//     const bgVideoList = document.querySelectorAll('.bg-video');
+//     const trailers = document.querySelectorAll('.trailer');
+//     const models = document.querySelectorAll('.model');
+
+//     bgVideoList.forEach(video => {
+//         video.classList.remove('.active');
+//         if(video.classList.contains(name)){
+//             video.classList.add('active')
+//         }
+//     });
+// }
